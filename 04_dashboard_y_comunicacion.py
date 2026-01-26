f9dc231bd8364d75b79af4e332ce6014b8fa3b79
Rodrigo-electro <rodrigoalvarezacampos@gmail.com>
2026-01-25 21:54:48 -0600

    Update 04_dashboard_y_comunicacion.py

--- 04_dashboard_y_comunicacion.py
+++ 04_dashboard_y_comunicacion.py
@@ -18,7 +18,7 @@ Original file is located at
 ## Configuración del entorno
 """
 
-!pip install plotly streamlit scikit-learn pandas numpy joblib
+#!pip install plotly streamlit scikit-learn pandas numpy joblib
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt

628a1ab35aa7dff42eb0bb7911374bdf2e74da94
Rodrigo-electro <rodrigoalvarezacampos@gmail.com>
2026-01-25 21:55:35 -0600

    Update 04_dashboard_y_comunicacion.py

--- 04_dashboard_y_comunicacion.py
+++ 04_dashboard_y_comunicacion.py
@@ -21,7 +21,7 @@ Original file is located at
 #!pip install plotly streamlit scikit-learn pandas numpy joblib
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 import plotly.graph_objects as go

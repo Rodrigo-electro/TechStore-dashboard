9cd1764e65002cb317653e107a7f66ef79d7efeb
Rodrigo-electro <rodrigoalvarezacampos@gmail.com>
2026-01-27 18:30:30 -0600

    APP_VERSION2.py

--- APP_VERSION2.py
+++ APP_VERSION2.py
@@ -99,7 +99,7 @@ df_inv = calcular_inventario(df_trans, df_prod)
 
 # --- SIDEBAR DE NAVEGACIÓN ---
 with st.sidebar:
-    st.image("https://www.flaticon.com/free-icon/money-growth_12149250?term=sales&page=1&position=16&origin=search&related_id=12149250", width=60)
+    st.image("https://www.flaticon.es/icono-gratis/ventas_994649?term=ventas&page=1&position=2&origin=tag&related_id=994649", width=60)
     st.title("TechStore ERP")
     
     perfil = st.radio("Seleccione Módulo:", 
